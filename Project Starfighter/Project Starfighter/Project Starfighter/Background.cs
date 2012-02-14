000d89ff14565a9cbff106e053a11a696392d2b6
Aarao Cabral Gouveia <acabralg@uark.edu>
2012-02-13 18:20:03 -0600

    background, and ship added now

---
+++ Project Starfighter/Project Starfighter/Project Starfighter/Background.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+
+namespace WindowsGame1
+{
+    public class  Background
+    {
+        // Textures to hold the two background images
+        Texture2D t2dBackground, t2dParallax;
+
+        // the next two lines determine how large the background will have when displayed
+        int iViewportWidth = 1280;
+        int iViewportHeight = 720;
+
+        // the next four lines hold the width and height of the background images. 
+        int iBackgroundWidth = 1920;
+        int iBackgroundHeight = 720;
+
+        int iParallaxWidth = 1680;
+        int iParallaxHeight = 480;
+
+        // the next two lines defines how many pixels before the screen begins is the background image going to start
+        int iBackgroundOffset;
+        int iParallaxOffset;
+
+        public int BackgroundOffset
+        {
+            get { return iBackgroundOffset; }
+            set
+            {
+                iBackgroundOffset = value;
+                if (iBackgroundOffset < 0)
+                {
+                    iBackgroundOffset += iBackgroundWidth;
+                }
+                if (iBackgroundOffset > iBackgroundWidth)
+                {
+                    iBackgroundOffset -= iBackgroundWidth;
+                }
+            }
+        }
+
+        public int ParallaxOffset
+        {
+            get { return iParallaxOffset; }
+            set
+            {
+                iParallaxOffset = value;
+                if (iParallaxOffset < 0)
+                {
+                    iParallaxOffset += iParallaxWidth;
+                }
+                if (iParallaxOffset > iParallaxWidth)
+                {
+                    iParallaxOffset -= iParallaxWidth;
+                }
+            }
+        }
+
+        // Determines if we will draw the Parallax overlay.
+        bool drawParallax = true;
+
+        public bool DrawParallax
+        {
+            get { return drawParallax; }
+            set { drawParallax = value; }
+        }
+
+        // Constructor when passed a Content Manager and two strings
+        public Background(ContentManager content,
+                          string sBackground,
+                          string sParallax)
+        {
+
+            t2dBackground = content.Load<Texture2D>(sBackground);
+            iBackgroundWidth = t2dBackground.Width;
+            iBackgroundHeight = t2dBackground.Height;
+            t2dParallax = content.Load<Texture2D>(sParallax);
+            iParallaxWidth = t2dParallax.Width;
+            iParallaxHeight = t2dParallax.Height;
+        }
+
+        // Constructor when passed a content manager and a single string
+        public Background(ContentManager content, string sBackground)
+        {
+
+            t2dBackground = content.Load<Texture2D>(sBackground);
+            iBackgroundWidth = t2dBackground.Width;
+            iBackgroundHeight = t2dBackground.Height;
+            t2dParallax = t2dBackground;
+            iParallaxWidth = t2dParallax.Width;
+            iParallaxHeight = t2dParallax.Height;
+            drawParallax = false;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Draw the background panel, offset by the player's location
+            spriteBatch.Draw(   t2dBackground,
+                                new Rectangle(-1 * iBackgroundOffset,
+                                0, iBackgroundWidth,
+                                iViewportHeight),
+                                Color.White);
+
+            // If the right edge of the background panel will end 
+            // within the bounds of the display, draw a second copy 
+            // of the background at that location.
+            if (iBackgroundOffset > iBackgroundWidth - iViewportWidth)
+            {
+                spriteBatch.Draw(   t2dBackground,
+                                    new Rectangle(
+                                    (-1 * iBackgroundOffset) + iBackgroundWidth,
+                                    0,
+                                    iBackgroundWidth,
+                                    iViewportHeight),
+                                    Color.White);
+            }
+
+            if (drawParallax)
+            {
+                // Draw the parallax star field
+                spriteBatch.Draw(   t2dParallax,
+                                    new Rectangle(-1 * iParallaxOffset,
+                                    0, iParallaxWidth,
+                                    iViewportHeight),
+                                    Color.SlateGray);
+                // if the player is past the point where the star 
+                // field will end on the active screen we need 
+                // to draw a second copy of it to cover the 
+                // remaining screen area.
+                if (iParallaxOffset > iParallaxWidth - iViewportWidth)
+                {
+                    spriteBatch.Draw(   t2dParallax,
+                                        new Rectangle(
+                                        (-1 * iParallaxOffset) + iParallaxWidth,
+                                        0,
+                                        iParallaxWidth,
+                                        iViewportHeight),
+                                        Color.SlateGray);
+                }
+            }
+        }
+    }
+}
---
+++ Project Starfighter/Project Starfighter/Project Starfighter/Player.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace WindowsGame1
+{
+    class Player
+    {
+        AnimatedSprite asSprite; // the ship imabe
+        
+        // the next two lines determin the location of the ship on the screen
+        int iX = 604;
+        int iY = 260;
+
+        int iFacing = 0; // what direction the player is facing right = 0, left = 1
+        
+        bool bThrusting = false; // represents if the ship is moving in a direction
+
+        int iScrollRate = 0; // speed and direction that ship is moving positive value = right negative value = left
+                             // the bigger the number more pixels per update frame the screen will move
+
+        int iShipAccelerationRate = 1; // how fast iScrollRate can change
+
+        int iShipVerticalMoveRate = 3; // number of pixels the ship moves vertically when the player presses up/down
+
+        // the next two lines determin how fast iShipAccelerationRate can be applied.
+        float fSpeedChangeCount = 0.0f; // accumulates the time since the last speed change When it is greater than fSpeedChangeDelay, 
+                                        // the speed is allowed to change and will be reset to 0 if it does.
+        float fSpeedChangeDelay = 0.1f;
+
+        // the next two lines work in the same way that fSpeedChangeCounnt/Delay work except that they serve to limit how quickly the player 
+        // can move vertically. The value we have set here (0.01 seconds) is very, very low so we are basically just using this to keep the ship 
+        // moving at a consistant speed on fast computers.
+        float fVerticalChangeCount = 0.0f;
+        float fVerticalChangeDelay = 0.01f;
+
+
+        public int X
+        {
+            get { return iX; }
+            set { iX = value; }
+        }
+
+        public int Y
+        {
+            get { return iY; }
+            set { iY = value; }
+        }
+
+        public int Facing
+        {
+            get { return iFacing; }
+            set { iFacing = value; }
+        }
+
+        public bool Thrusting
+        {
+            get { return bThrusting; }
+            set { bThrusting = value; }
+        }
+
+        public int ScrollRate
+        {
+            get { return iScrollRate; }
+            set { iScrollRate = value; }
+        }
+
+        public int AccelerationRate
+        {
+            get { return iShipAccelerationRate; }
+            set { iShipAccelerationRate = value; }
+        }
+
+        public int VerticalMovementRate
+        {
+            get { return iShipVerticalMoveRate; }
+            set { iShipVerticalMoveRate = value; }
+        }
+
+        public float SpeedChangeCount
+        {
+            get { return fSpeedChangeCount; }
+            set { fSpeedChangeCount = value; }
+        }
+
+        public float SpeedChangeDelay
+        {
+            get { return fSpeedChangeDelay; }
+            set { fSpeedChangeDelay = value; }
+        }
+
+        public float VerticalChangeCount
+        {
+            get { return fVerticalChangeCount; }
+            set { fVerticalChangeCount = value; }
+        }
+
+        public float VerticalChangeDelay
+        {
+            get { return fVerticalChangeDelay; }
+            set { fVerticalChangeDelay = value; }
+        }
+
+        // returns a new rectangle based on the position and size of our ship.
+        public Rectangle BoundingBox
+        {
+            get { return new Rectangle(iX, iY, 72, 16); }
+        }
+
+        // constructor
+        public Player(Texture2D texture)
+        {
+            asSprite = new AnimatedSprite(texture, 0, 0, 72, 16, 4);
+            asSprite.IsAnimating = false;
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            asSprite.Draw(sb, iX, iY, false);
+        }
+
+        // update animation frames based on the values of iFacing and bThrusting.
+        public void Update(GameTime gametime)
+        {
+            if (iFacing == 0)
+            {
+                if (bThrusting)
+                    asSprite.Frame = 1;
+                else
+                    asSprite.Frame = 0;
+            }
+            else
+            {
+                if (bThrusting)
+                    asSprite.Frame = 3;
+                else
+                    asSprite.Frame = 2;
+            }
+        }
+    }
+}
