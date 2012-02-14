ad2b7c380e9aaa4f8cbe0ad1a9e4b074c2f0e9a4
Aarao Cabral Gouveia <acabralg@uark.edu>
2012-02-13 18:25:16 -0600

    adding what will be test for background

---
+++ Project Starfighter/Project Starfighter Test/Background_Tests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Microsoft.Xna.Framework.Content;
+using System.Windows.Forms;
+using Microsoft.Xna.Framework.Graphics;
+using Rhino.Mocks;
+
+
+
+
+
+
+namespace WindowsGame1.NUnitTest
+{
+    
+    [TestFixture] // tell NUnit that the class contains tests
+    class Background_Tests
+    {
+        private ContentManager content;
+        private Background background;
+        
+
+        [SetUp]
+        public void Initialize()
+        {
+           
+            content = MockRepository.GenerateStub<ContentManager>(); // create a mock contentManager
+
+            
+
+            background = new Background(    content,
+                                            @"Textures\PrimaryBackground",
+                                            @"Textures\ParallaxStars");
+
+        }
+        public void BackgroundOffset_test()
+        {
+            background.BackgroundOffset = -1;
+            Assert.AreEqual(1919, background.BackgroundOffset);
+        }
+        
+
+        
+
+        
+    }
+}
